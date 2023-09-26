19faa61c974c04bf7dbabf6ff5b6ed00cc995d25
julioc98 <julioc.santos98@gmail.com>
2023-09-26 00:03:31 -0300

    remove deprecated method

--- pkg/rpc/grpc_conn.go
+++ pkg/rpc/grpc_conn.go
@@ -1,12 +1,17 @@
 // Package rpc provides a gRPC client.
 package rpc
 
-import "google.golang.org/grpc"
+import (
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
 
 // GrpcConn creates a gRPC client.
 func GrpcConn() (*grpc.ClientConn, error) {
-	// Connect to a server
-	conn, err := grpc.Dial("query:50051", grpc.WithInsecure())
+	// Configure gRPC client to connect securely.
+	creds := credentials.NewClientTLSFromCert(nil, "")
+
+	conn, err := grpc.Dial("query:50051", grpc.WithTransportCredentials(creds))
 	if err != nil {
 		return nil, err
 	}
